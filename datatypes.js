a76823c5071e0e39769c0d1b93f17befa6f2f18d
rohandevpal <devrohanpal@gmail.com>
2026-03-13 22:50:46 +0530

    datatypes

---
+++ datatypes.js
@@ -0,0 +1,37 @@
+//• String, number aur boolean variables create karo.
+let string = "rohan";
+let number = 2;
+let bool = true;
+
+
+//typeof operator use karke type print karo.
+
+console.log(typeof(string));
+
+console.log(typeof(number));
+
+console.log(typeof(bool));
+
+
+//• Ek array aur ek object create karo.
+let arr = ["rohan", "pal", 34];
+let obj = {
+    name: "rohan",
+    age: 28,
+    edu: "BCA"
+}
+
+console.log(arr,obj);
+
+
+//• null aur undefined ka example banao.
+let a  = null;
+let b;
+
+console.log(a);
+console.log(b);
+
+
+//• '5' + 2 ka output check karo.
+
+console.log("5"+2);
