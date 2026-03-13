99e61f26bd6bcf99c4b83aa93603ef2b024ae1f9
rohandevpal <devrohanpal@gmail.com>
2026-03-13 23:03:09 +0530

    operators

--- operators.js
+++ operators.js
@@ -1,6 +1,6 @@
 //• Do numbers ka addition karo.
-let a = 6;
-let b = 7;
+let a = 2;
+let b = '2';
 console.log(a+b);
 
 
@@ -17,3 +17,27 @@ function oddEven(num){
 oddEven(97);
 
 //• >, <, ==, === comparison test karo.
+
+if(a > b){
+    console.log('a is bigger than b');
+
+}else{
+    console.log('b is bigger than a');
+    
+}
+
+if(a == b){
+    console.log('both have same value');
+    
+}else{
+    console.log('both does not have same value');
+    
+}
+
+if(a === b){
+    console.log('both have same value and same data type');
+    
+}else{
+    console.log('both does not have same value and not even same data type');
+    
+}
