7cdc643a3c3035d6d8036beb290e8239727aba47
rohandevpal <devrohanpal@gmail.com>
2026-03-13 22:57:17 +0530

    operators

---
+++ operators.js
@@ -0,0 +1,19 @@
+//• Do numbers ka addition karo.
+let a = 6;
+let b = 7;
+console.log(a+b);
+
+
+//• % operator use karke even/odd check karo.
+function oddEven(num){
+    if(num % 2 === 0){
+        console.log(num,'is odd');
+        
+    }else{
+        console.log(num, 'is even');
+        
+    }
+}
+oddEven(97);
+
+//• >, <, ==, === comparison test karo.
