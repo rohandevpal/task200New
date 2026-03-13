550af7b2fdfd75e7d7328f4c6729d5bfa1ee9e5e
rohandevpal <devrohanpal@gmail.com>
2026-03-13 22:42:59 +0530

    variabels

---
+++ variables.js
@@ -0,0 +1,25 @@
+//• Ek variable 'name' banao aur usme apna naam store karke console me print karo.
+
+let name = "Rohan pal";
+console.log(name);
+
+
+//• let use karke do numbers add karo.
+
+let num1 = 1;
+let num2 = 3;
+
+console.log(num1+num2);
+
+
+//const me PI value store karo.
+const PI = 3.14;
+console.log(PI);
+
+//• Age variable banao aur message print karo: My age is ...
+let age = 26;
+console.log("my age is ",age);
+
+
+
+
