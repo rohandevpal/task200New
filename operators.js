5c4c314869e2ff32ee78f1a7f2dbb672d3cf6d34
rohandevpal <devrohanpal@gmail.com>
2026-03-14 18:11:15 +0530

    operators updated

--- operators.js
+++ operators.js
@@ -1,7 +1,7 @@
 //• Do numbers ka addition karo.
 let a = 2;
 let b = '2';
-console.log(a+b);
+// console.log(a+b);
 
 
 //• % operator use karke even/odd check karo.
@@ -14,7 +14,7 @@ function oddEven(num){
         
     }
 }
-oddEven(97);
+// oddEven(97);
 
 //• >, <, ==, === comparison test karo.
 
@@ -41,7 +41,7 @@ function compareValues(a, b) {
   }
   
   // Function call
-  compareValues(10, 5);
+  // compareValues(10, 5);
 
 
   //• Simple calculator logic likho (add, subtract, multiply).
@@ -53,4 +53,107 @@ function compareValues(a, b) {
     
   }
 
-  calc(6,9)
+  // calc(6,9)
+
+
+//Comparison Operator – Check whether 10 and 20 are equal using a comparison operator. Expected Output: false
+
+function checkEqualNumbers (a, b){
+  if(a == b){
+    console.log(true);
+    
+  }else{
+    console.log(false);
+    
+  }
+}
+
+checkEqualNumbers(10, 20);
+
+//Greater Than Comparison – Check if age is greater than 18. Expected Output: true
+
+function checkAge(age){
+  if(age > 18){
+    console.log(true);
+    
+  }else{
+    console.log(false);
+    
+  }
+}
+checkAge(23);
+
+//Logical AND – Check if user is logged in AND user is admin. Expected Output: Access granted: true
+function userCheck(isLoggedin, isAdmin){
+    return isLoggedin && isAdmin
+}
+let accessGranted = userCheck(true, true);
+
+console.log("Access Granted:", accessGranted);
+
+
+
+//Logical OR – Check if user is admin OR moderator. Expected Output: Access allowed: true
+
+function useCheckAdminModerator(isAdmin, isModerator){
+  return isAdmin || isModerator;
+
+}
+
+let accessAdmin = useCheckAdminModerator(false, true);
+
+console.log("Access Allowed:", accessAdmin);
+
+// Multiple Arithmetic Operations – Take three numbers and calculate (a + b) * c. Expected Output: Result: 60
+let number1 = 5;
+let number2 = 7;
+let number3 = 5;
+
+console.log((number1+number2)*number3);
+
+//by using function
+
+function calculateMultiples(a,b,c){
+  return (a+b)*c;
+}
+let result = calculateMultiples(5,7,5);
+console.log('result:', result);
+
+
+
+
+//. Comparison + Logical – Check if age > 18 AND salary > 20000. Expected Output: Eligible:true
+
+function checkSalary(age, salary){
+  if(age > 18 && salary > 20000){
+    console.log('Eligible', true);
+    
+  }else{
+    console.log('Eligible:', false);
+    
+  }
+}
+
+checkSalary(24, 50000);
+
+//Assignment Chain – Store 20 and subtract 5 using -= operator. Expected Output: Finalvalue: 15
+
+function plusMinus(store, subtract){
+  store -= subtract;
+  return store
+}
+
+let resultValue = plusMinus(20, 5) 
+console.log(resultValue);
+
+//Multiple Conditions – Check if a number is greater than 10 and less than 50. Expected Output: true
+function greterAndLess(a,b){
+  if(a > 10 && a <50){
+    console.log(true);
+    
+  }else{
+    console.log(false);
+    
+  }
+}
+greterAndLess(23)
